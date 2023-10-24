8c79210fabdfde77cdec50aec965231d8a2a8403
Pablo <muruapablo@gmail.com>
2023-10-23 21:41:36 -0300

    cambios2 html

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
-# Create your models here.
+class Paleta(models.Model):
+    marca = models.CharField (max_length=30)
+    descripcion = models.TextField()
+    anio = models.IntegerField()

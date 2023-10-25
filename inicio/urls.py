1a1db19f76b8b78c5aa33a1dd8fe24614d6e5afd
Pablo <muruapablo@gmail.com>
2023-10-24 23:54:15 -0300

    cambios38 html

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from inicio.views import inicio, paletas
+
 urlpatterns = [
-    path('', inicio),
-    path('paletas/', paletas),
-    ]
+    path('', inicio, name='inicio'),
+    path('paletas/', paletas, name='paletas'),
+]

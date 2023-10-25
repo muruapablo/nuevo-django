59db591fafdf66f60f43b218e4556a9be8fc6dc4
Pablo <muruapablo@gmail.com>
2023-10-25 00:28:49 -0300

    cambios40 html

--- inicio/models.py
+++ inicio/models.py
@@ -3,3 +3,7 @@ class Paleta(models.Model):
     marca = models.CharField (max_length=30)
     descripcion = models.TextField()
     anio = models.IntegerField()
+   
+    def __str__(self):
+        return f' {self.id} - {self.marca} - {self.descripcion} - {self.anio}' 
+    
